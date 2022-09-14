7042daf598c1568505fbaf66b5157b49fb85fbac
Arroyo <arroyolokura8@gmail.com>
2022-09-14 16:05:02 -0500

    Proyecto con la estructura se corrigieron errores

--- src/main/java/com/arroyo/cine/repository/PeliculaSerieRepository.java
+++ src/main/java/com/arroyo/cine/repository/PeliculaSerieRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PeliculaSerieRepository extends JpaRepository<Integer, PeliculaSerie> {
+public interface PeliculaSerieRepository extends JpaRepository<PeliculaSerie, Integer> {
 }
